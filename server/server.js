27ac7b62ad4840a1080c36a4bbeef30a787ce63e
denysbotnar <denysbotnar@gmail.com>
2019-10-24 14:03:36 +0300

    feat: BE http logger

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const routes = require('./routes');
 
 const app = express();
 const { API_PORT } = process.env;
 
 app.use(bodyParser.json());
-app.use('/v1', routes);
+app.use(morgan('tiny'));
+app.use('/api/v1', routes);
 
 app.listen(API_PORT, () => {
   console.log(`Example app listening on port ${API_PORT}!`);
